4a7e5bb86db340ef2e7eeee7405b8106085658de
Vahram Grigoryan <ramgrigoryan@gmail.com>
2022-03-28 11:22:46 +0400

    working with response clear

--- http-01-starting-setup/assets/scripts/app.js
+++ http-01-starting-setup/assets/scripts/app.js
@@ -7,7 +7,6 @@ xhr.onload = function (){
     const listOfPosts = JSON.parse(xhr.response);
     for(const post of listOfPosts){
         const postEl = document.importNode(singlePost.content,true);
-        console.log(postEl);
         postEl.querySelector("h2").textContent = post.title;
         postEl.querySelector("p").textContent = post.body;
         posts.append(postEl);
