818deb5068d7641be16fc3348e96aeabec16bb66
Vahram Grigoryan <ramgrigoryan@gmail.com>
2022-03-28 11:21:34 +0400

    working with response

--- http-01-starting-setup/assets/scripts/app.js
+++ http-01-starting-setup/assets/scripts/app.js
@@ -1,3 +1,16 @@
+const posts = document.querySelector(".posts");
+const singlePost = document.getElementById("single-post");
+
 const xhr = new XMLHttpRequest();
 xhr.open("GET","https://jsonplaceholder.typicode.com/posts");
+xhr.onload = function (){
+    const listOfPosts = JSON.parse(xhr.response);
+    for(const post of listOfPosts){
+        const postEl = document.importNode(singlePost.content,true);
+        console.log(postEl);
+        postEl.querySelector("h2").textContent = post.title;
+        postEl.querySelector("p").textContent = post.body;
+        posts.append(postEl);
+    }
+}
 xhr.send();
